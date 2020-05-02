2f768a0003f38804ca32a2a5d7f4a77a2c4994c7
AdamA <abonyi@seznam.cz>
2020-05-02 17:53:50 +0200

    first web change

--- src/pages/example/_components/RoutifyIntro.svelte
+++ src/pages/example/_components/RoutifyIntro.svelte
@@ -1,6 +1,3 @@
-<h1>Routify Starter</h1>
+<h1>Adams Sveltejs Playground</h1>
 
-To see an example app, go to 
 <a href="/example">/example</a>
-
-<p>To delete the example app, simply delete the ./src/pages/example folder.</p>
