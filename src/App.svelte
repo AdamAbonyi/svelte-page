5392cfe325d479db07ec92d47499c8d3147f3c1e
Adam Abonyi <adam.abonyi@summitmedia.com>
2020-05-08 00:59:47 +0200

    add web font loader

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,17 @@
   import { routes } from '@sveltech/routify/tmp/routes'
 
   import { setupI18n } from './services/i18n'
-  
+
   setupI18n({ withLocale: 'en' })
+
+  import WebFont from 'webfontloader';
+
+  WebFont.load({
+    google: {
+      families: ['Roboto', 'Monoton']
+    }
+  });
+
 </script>
 
 <Router {routes} />
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -19,12 +19,39 @@
 
   main {
     grid-column: span 12;
-    
+
   }
 
   footer {
     grid-column: span 12;
   }
+
+  span {
+    color: orange;
+    font-family: Monoton;
+    font-size: 8rem;
+    text-decoration: none;
+    -webkit-transition: all 0.5s;
+    -moz-transition: all 0.5s;
+    transition: all 0.5s;
+  }
+
+  span:hover {
+    color: #ffffff;
+  -webkit-animation: neon5 1.5s ease-in-out infinite alternate;
+  -moz-animation: neon5 1.5s ease-in-out infinite alternate;
+  animation: neon5 1.5s ease-in-out infinite alternate;
+}
+
+@keyframes neon5 {
+  from {
+    text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #FF9900, 0 0 70px #FF9900, 0 0 80px #FF9900, 0 0 100px #FF9900, 0 0 150px #FF9900;
+  }
+  to {
+    text-shadow: 0 0 5px #fff, 0 0 10px #fff, 0 0 15px #fff, 0 0 20px #FF9900, 0 0 35px #FF9900, 0 0 40px #FF9900, 0 0 50px #FF9900, 0 0 75px #FF9900;
+  }
+}
+
 </style>
 
 <Octocat />
@@ -38,5 +65,7 @@
     <slot />
   </main>
 
-  <footer>FOOTER</footer>
+  <footer>
+  <span>FOOTER</span>
+  </footer>
 </div>
